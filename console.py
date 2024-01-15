6baca8da79bc00f9c3fa2d89244382b68bfd54ac
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-14 23:23:46 -0800

    changes

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}' \
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -117,7 +117,6 @@ class HBNBCommand(cmd.Cmd):
         """Create an object of any class"""
         if not args:
             print("** class name missing **")
-<<<<<<< HEAD
             return
         elif args not in HBNBCommand.classes:
             print("** class doesn't exist **")
@@ -127,37 +126,34 @@ class HBNBCommand(cmd.Cmd):
         storage.save()
         print(new_instance.id)
         storage.save()
-=======
-        return
-    
-    args_array = args.split()
-    class_name = args_array[0]
-
-    if class_name not in HBNBCommand.classes:
-        print("** class doesn't exist **")
         return
 
-    new_instance = HBNBCommand.classes[class_name]()
+        args_array = args.split()
+        class_name = args_array[0]
 
-    for param_index in range(1, len(args_array)):
-        param_array = args_array[param_index].split("=")
+        if class_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
 
-        if len(param_array) == 2:
-            key = param_array[0]
+        new_instance = HBNBCommand.classes[class_name]()
+        for param_index in range(1, len(args_array)):
+            param_array = args_array[param_index].split("=")
 
-            if key not in HBNBCommand.valid_keys[class_name]:
-                continue
+            if len(param_array) == 2:
+                key = param_array[0]
 
-            value = self.parse_value(param_array[1])
+                if key not in HBNBCommand.valid_keys[class_name]:
+                    continue
 
-            if value is not None:
-                setattr(new_instance, key, value)
-        else:
-            pass
+                value = self.parse_value(param_array[1])
 
-    new_instance.save()
-    print(new_instance.id)
->>>>>>> 05b745d327932309b73a99d12e0a5bfa232790dc
+                if value is not None:
+                    setattr(new_instance, key, value)
+                else:
+                    pass
+
+        new_instance.save()
+        print(new_instance.id)
 
     def help_create(self):
         """ Help information for the create method """
@@ -352,6 +348,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
