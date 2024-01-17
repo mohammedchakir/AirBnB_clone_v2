2928714c933f69a04d7f1d4f35ea6f94ee5edd06
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-17 23:42:14 +0100

    appending new files

--- models/city.py
+++ models/city.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """This module defines the City class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """A City class for hbnb"""
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='city', cascade='all, delete-orphan')
--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines the User class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """A User class for hbnb"""
@@ -10,4 +11,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-
+    reviews = relationship('Review', backref='user', cascade='all, delete-orphan')
