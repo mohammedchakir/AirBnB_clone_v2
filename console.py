abf1bd320a767b726e665869d59fc6814644eaa1
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-18 02:37:13 +0100

    add solution

--- console.py
+++ console.py
@@ -7,7 +7,7 @@ import uuid
 import os.path
 from datetime import datetime
 from models.base_model import BaseModel
-from models.__init__ import storage
+from models import storage
 from models.user import User
 from models.place import Place
 from models.state import State
@@ -159,12 +159,12 @@ class HBNBCommand(cmd.Cmd):
             return
 
         if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-            if not hasattr(obj_kwargs, 'id'):
-                obj_kwargs['id'] = str(uuid.uuid4())
-            if not hasattr(obj_kwargs, 'created_at'):
-                obj_kwargs['created_at'] = str(datetime.now())
-            if not hasattr(obj_kwargs, 'updated_at'):
-                obj_kwargs['updated_at'] = str(datetime.now())
+            if not hasattr(object_kwargs, 'id'):
+                object_kwargs['id'] = str(uuid.uuid4())
+            if not hasattr(object_kwargs, 'created_at'):
+                object_kwargs['created_at'] = str(datetime.now())
+            if not hasattr(object_kwargs, 'updated_at'):
+                object_kwargs['updated_at'] = str(datetime.now())
 
             new_instance = HBNBCommand.classes[class_name](**object_kwargs)
             new_instance.save()
