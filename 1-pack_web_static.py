590e6e1957d87dd9993512636f854ef95037088c
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-01-31 23:06:54 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,19 +5,19 @@ of the web_static folder
 """
 from fabric.api import local
 from datetime import datetime
+from time import strftime
 
 
 def do_pack():
     """
     Creates a .tgz archive from the contents of the web_static folder
     """
-    try:
-        current_time = datetime.utcnow().strftime("%Y%m%d%H%M%S")
-        archive_name = "web_static_{}.tgz".format(current_time)
-        
+        current_time = strftime("%Y%m%d%H%M%S")
+        archive_name = "versions/web_static_{}.tgz".format(current_time)
+    try: 
         local("mkdir -p versions")
         local("tar -cvzf versions/{} -C web_static".format(archive_name))
 
-        return "versions/{}".format(archive_name)
+        return archive_name
     except Exception as e:
         return None
