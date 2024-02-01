1734efddb39820126ca3f009645892de3ad69bef
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-02 00:47:56 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 # This script sets up web servers for deployment of web_static
-
 # Install Nginx if not already installed
 sudo apt-get update
 sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-# Create necessary folders if they don't exist
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-
+sudo touch /data/web_static/releases/test/index.html
 # Create a fake HTML file
-echo "<html>
+sudo echo "<html>
     <head>
     </head>
     <body>
@@ -19,32 +21,13 @@ echo "<html>
  </html>" | sudo tee /data/web_static/releases/test/index.html
 
 # Create symbolic link /data/web_static/current
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of /data/ to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
-config_content="
-server {
-    listen 80;
-    server_name _;
-
-    location /hbnb_static {
-        alias /data/web_static/current/;
-    }
-
-    error_page 404 /404.html;
-    location = /404.html {
-        root /usr/share/nginx/html;
-        internal;
-    }
-
-    add_header X-Served-By $HOSTNAME;
-}
-"
-
-echo "$config_content" | sudo tee /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 # Restart Nginx to apply changes
 sudo service nginx restart
