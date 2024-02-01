6f3d0990f508c83d3bc2847d0505d4355a820d07
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-02 00:22:29 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,7 +4,7 @@ Fabric script that generates a .tgz archive from the contents
 of the web_static folder
 """
 from fabric.api import local
-from datetime import datetime
+from datetime import date
 from time import strftime
 
 
