65787c1cd94069b68df86eff735fa438853c95f4
mohammedchkir <zerroukimedchkir@gmail.com>
2024-02-01 15:32:07 -0800

    add

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,8 +12,8 @@ def do_pack():
     """
     Creates a .tgz archive from the contents of the web_static folder
     """
-        current_time = strftime("%Y%m%d%H%M%S")
-    try: 
+    current_time = strftime("%Y%m%d%H%M%S")
+    try:
         local("mkdir -p versions")
         local("tar -czvf versions/web_static_{}.tgz web_static/"
               .format(current_time))
