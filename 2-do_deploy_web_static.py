3391c7a2693e310e5f7f4077e1d14c1054a28325
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-04 17:08:24 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,6 +4,7 @@ Fabric script that distributes an archive to your web servers
 """
 from fabric.api import env, run, put
 import os
+from datetime import datetime
 
 env.hosts = ['18.233.62.225', '52.91.116.153']
 env.user = 'ubuntu'
@@ -11,44 +12,6 @@ env.key_filename = ['~/.ssh/school']
 
 
 def do_deploy(archive_path):
-<<<<<<< HEAD
-    """
-    Distributes an archive to web servers.
-
-    :param archive_path: Path to the archive to be deployed.
-    :return: True if successful, False otherwise.
-    """
-    if not os.path.exists(archive_path):
-        print(f"Error: Archive not found at {archive_path}")
-        return False
-
-    try:
-        archive_filename = os.path.basename(archive_path)
-        archive_no_ext = os.path.splitext(archive_filename)[0]
-        remote_tmp_path = '/tmp/'
-        remote_release_path = '/data/web_static/releases/'
-        remote_current_path = '/data/web_static/current'
-
-        # Upload archive to /tmp/ directory of the web server
-        put(archive_path, remote_tmp_path)
-
-        # Uncompress the archive to /data/web_static/releases/
-        run(f"mkdir -p {remote_release_path}{archive_no_ext}")
-        run(f"tar -xzf {remote_tmp_path}{archive_filename} -C \
-                {remote_release_path}{archive_no_ext}")
-
-        # Delete the archive from the web server
-        run(f"rm {remote_tmp_path}{archive_filename}")
-
-        # Delete the symbolic link /data/web_static/current from the web server
-        run(f"rm -f {remote_current_path}")
-
-        # Create a new symbolic link /data/web_static/current linked to version
-        run(f"ln -s {remote_release_path}{archive_no_ext} \
-                {remote_current_path}")
-
-        print("Deployment successful.")
-=======
         """Deploy web files to server
         """
         try:
@@ -90,5 +53,4 @@ web_static_{}/ /data/web_static/current'.format(timestamp))
                 return False
 
         # return True on success
->>>>>>> cf190dc211eae8eaec8caba6b276072a05eb4bad
         return True
