73e7edb81e1a7f86832b3eb57e67adfc05255828
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-04 22:07:50 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,8 +8,8 @@ sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 # Create a fake HTML file
 sudo echo "<html>
@@ -22,12 +22,9 @@ sudo echo "<html>
 
 # Create symbolic link /data/web_static/current
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-
 # Give ownership of /data/ to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
-
 # Update Nginx configuration
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-
 # Restart Nginx to apply changes
 sudo service nginx restart
