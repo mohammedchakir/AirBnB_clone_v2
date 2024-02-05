9fce9dcc2cd086f5990600558c856be13d5ce319
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-05 01:07:17 +0100

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,35 +2,31 @@
 """
 Fabric script that deletes out-of-date archives
 """
-from fabric.api import task, local, env, put, run, runs_once
-from fabric.state import commands, connections
+from fabric.api import *
 import os.path
 
-env.user = 'ubuntu'
 env.hosts = ['18.233.62.225', '52.91.116.153']
-env.key_filename = "~/id_rsa"
 
 
 def do_clean(number=0):
-    """deletes out-of-date archives"""
-    local('ls -t ~/AirBnB_Clone_V2/versions/').split()
+    """Delete out-of-date archives.
+
+    Args:
+        number (int): The number of archives to keep.
+
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
     with cd("/data/web_static/releases"):
-        target_R = sudo("ls -t .").split()
-    paths = "/data/web_static/releases"
-    number = int(number)
-    if number == 0:
-        num = 1
-    else:
-        num = number
-    if len(target_R) > 0:
-        if len(target) == number or len(target) == 0:
-            pass
-        else:
-            cl = target[num:]
-            for i in range(len(cl)):
-                local('rm -f ~/AirBnB_Clone_V2/versions/{}'.format(target[-1]))
-        rem = target_R[num:]
-        for j in range(len(rem)):
-            sudo('rm -rf {}/{}'.format(paths, rem[-1].strip(".tgz")))
-    else:
-        pass
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
