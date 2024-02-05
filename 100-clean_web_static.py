ffea0a2cef0137930350695ce1b879395c898b4b
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-05 01:00:18 +0100

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,72 +3,34 @@
 Fabric script that deletes out-of-date archives
 """
 from fabric.api import task, local, env, put, run, runs_once
-from datetime import datetime
-import os
+from fabric.state import commands, connections
+import os.path
 
+env.user = 'ubuntu'
 env.hosts = ['18.233.62.225', '52.91.116.153']
-
-
-def do_pack():
-    """ method doc
-        sudo fab -f 1-pack_web_static.py do_pack
-    """
-    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
-    mkdir = "mkdir -p versions"
-    path = "versions/web_static_{}.tgz".format(formatted_dt)
-    print("Packing web_static to {}".format(path))
-    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
-        return path
-    return None
-
-def do_deploy(archive_path):
-    """ method deploy
-    """
-    try:
-        if not os.path.exists(archive_path):
-            return False
-        fn_with_ext = os.path.basename(archive_path)
-        fn_no_ext, ext = os.path.splitext(fn_with_ext)
-        dpath = "/data/web_static/releases/"
-        put(archive_path, "/tmp/")
-        run("rm -rf {}{}/".format(dpath, fn_no_ext))
-        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
-        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
-        run("rm /tmp/{}".format(fn_with_ext))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
-        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
-        print("New version deployed!")
-        return True
-    except Exception:
-        return False
-
-
-def deploy():
-    """ method doc
-        sudo fab -f 1-pack_web_static.py do_pack
-    """
-    path = do_pack()
-    if path is None:
-        return False
-    return do_deploy(path)
-
-
-def remove_local(number):
-    """ method doc
-        sudo fab -f 1-pack_web_static.py do_pack
-    """
-    local("ls -dt versions/* | tail -n +{} | sudo xargs rm -fr".format(number))
+env.key_filename = "~/id_rsa"
 
 
 def do_clean(number=0):
-    """ method doc
-        sudo fab -f 1-pack_web_static.py do_pack
-    """
-    if int(number) == 0:
-        number = 1
-    number = int(number) + 1
-    remove_local(number)
-    rem_path = "/data/web_static/releases/*"
-    run("ls -dt {} | tail -n +{} | sudo xargs rm -fr".format(rem_path, number))
+    """deletes out-of-date archives"""
+    local('ls -t ~/AirBnB_Clone_V2/versions/').split()
+    with cd("/data/web_static/releases"):
+        target_R = sudo("ls -t .").split()
+    paths = "/data/web_static/releases"
+    number = int(number)
+    if number == 0:
+        num = 1
+    else:
+        num = number
+    if len(target_R) > 0:
+        if len(target) == number or len(target) == 0:
+            pass
+        else:
+            cl = target[num:]
+            for i in range(len(cl)):
+                local('rm -f ~/AirBnB_Clone_V2/versions/{}'.format(target[-1]))
+        rem = target_R[num:]
+        for j in range(len(rem)):
+            sudo('rm -rf {}/{}'.format(paths, rem[-1].strip(".tgz")))
+    else:
+        pass
