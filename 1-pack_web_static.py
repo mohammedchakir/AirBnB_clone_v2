1c65fd65dda3030c80b18eb2afe1da609716c3bf
mohammedchakir <zerroukimedchakir@gmail.com>
2024-02-04 19:02:03 -0800

    adding

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,15 +10,15 @@ import os
 
 def do_pack():
     local('sudo mkdir -p versions')
-    
+
     t = datetime. now()
-    t_str = t.strftime('§Y%m%d%H%M&S')
-    
+    t_str = t.strftime('%Y%m%d%H%M%S')
+
     local(f'sudo tar -cvzf versions/web_static_{t_str}.tgz web_static')
-    
+
     f_path = f"versions/web_static_{t_str}.tgz"
     f_size = os. path.getsize(f_path)
-    
+
     print(f"web_static packed: {f_path} -> {f_size}Bytes")
-    
+
     return f_path
