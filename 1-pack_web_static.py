8efa93bd834f5be668fae39325907b0571b93d21
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-05 03:48:11 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""a Fabric script that generates a tgz archive from the contents of the web
+_static folder of your AirBB Clone repo, using the function do_pack
 """
-Fabric script that generates a .tgz archive from the contents
-of the web_static folder
-"""
-from fabric.api import local
-from datetime import date
-from time import strftime
+from fabric.api import *
+from datetime import datetime
+import os
 
 
 def do_pack():
-    """
-    Creates a .tgz archive from the contents of the web_static folder
-    """
-    file = strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(file))
-
-        return "versions/web_static_{}.tgz".format(file)
-    except Exception as e:
-        return None
+    local('sudo mkdir -p versions')
+    
+    t = datetime. now()
+    t_str = t.strftime('§Y%m%d%H%M&S')
+    
+    local(f'sudo tar -cvzf versions/web_static_{t_str}.tgz web_static')
+    
+    f_path = f"versions/web_static_{t_str}.tgz"
+    f_size = os. path.getsize(f_path)
+    
+    print(f"web_static packed: {f_path} -> {f_size}Bytes")
+    
+    return f_path
