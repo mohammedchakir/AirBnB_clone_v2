dd3c8c70f1f4e93a9e0cac95afc8179fca452c5b
mohammedchakir <zerroukimedchakir@gmail.com>
2024-02-16 03:22:16 +0100

    more templates

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"""
+This module starts a Flask web application.
+"""
+
+from flask import Flask, render_template
+from urllib.parse import unquote
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Display "Hello HBNB!" on the root route.
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """
+    Display "HBNB" on the '/hbnb' route.
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    """
+    Display "C " followed by the value of the text variable.
+    """
+    text = unquote(text)
+    text = text.replace("_", " ")
+    return 'C {}'.format(text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python_text(text='is cool'):
+    """
+    Display "Python " followed by the value of the text variable.
+    """
+    text = unquote(text.replace("_", " "))
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """
+    Display "n is a number" only if n is an integer.
+    """
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_number_template(n):
+    """
+    Display an HTML with "Number: n" inside an H1 tag in the BODY.
+    """
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def display_number_odd_or_even(n):
+    """
+    Display an HTML with "Number: n is even|odd" inside an H1 tag.
+    """
+    odd_or_even = 'even' if n % 2 == 0 else 'odd'
+    return render_template('6-number_odd_or_even.html', number=n, odd_or_even=odd_or_even)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
