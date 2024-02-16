34b676080dbb47684ed185c18eee11fa8c8dccd6
mohammedchakir <zerroukimedchakir@gmail.com>
2024-02-16 20:48:07 +0100

    add

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 -- Task3: Prepare MySQL server for the project
 -- Create or update the hbnb_dev user with the specified privileges
 -- Create or update the hbnb_dev_db database
-CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Ensure that the hbnb_dev user has privileges only on the specified database
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
