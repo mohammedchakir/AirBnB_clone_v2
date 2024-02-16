53515040f8e10bb23cd59df4430c4b7891208064
mohammedchakir <zerroukimedchakir@gmail.com>
2024-02-16 02:59:15 +0100

    adding solutions

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -45,5 +45,13 @@ def display_python_text(text='is cool'):
     return 'Python {}'.format(text)
 
 
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """
+    Display "n is a number" only if n is an integer.
+    """
+    return '{} is a number'.format(n)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
