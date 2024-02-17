9107148930331dc281625c48137c9cfc678c8a04
mohammedchakir <zerroukimedchakir@gmail.com>
2024-02-17 20:42:47 +0100

    add

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -5,33 +5,31 @@ This script starts a Flask web application with specified routes.
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_session(self):
+def close(self):
     """Remove the current SQLAlchemy Session after each request"""
     storage.close()
 
 
 @app.route('/states', strict_slashes=False)
-def display_states():
+def state():
     """Display a list of all State objects present in DBStorage"""
     states = storage.all(State)
-    return render_template('9-states.html', states=states)
+    return render_template('9-states.html', states=states, mode='all')
 
 
 @app.route('/states/<id>', strict_slashes=False)
-def display_state_cities(id):
+def state_by_id(id):
     """Display cities associated with a State"""
-    state = storage.get(State, id)
-    if state:
-        return render_template('9-states_by_id.html', state=state)
-    else:
-        return render_template('9-not_found.html')
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template('9-states.html', states=state, mode='id')
+    return render_template('9-states.html', states=state, mode='none')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port="5000")
