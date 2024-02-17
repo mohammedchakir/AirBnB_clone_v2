3971db3808b98c7e4d4b38b04d340c3c55e17485
mohammedchakir <zerroukimedchakir@gmail.com>
2024-02-18 00:32:08 +0100

    add

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,35 +1,28 @@
 #!/usr/bin/python3
 """
-This script starts a Flask web application with specified routes.
+This script starts a Flask web app with specified routes.
 """
-
-from flask import Flask, render_template
 from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """Display a HTML page with Airbnb filters"""
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
+
+
 @app.teardown_appcontext
-def teardown_session(exception):
+def teardown(exc):
     """Remove the current SQLAlchemy Session after each request"""
     storage.close()
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def display_hbnb_filters():
-    """Display a HTML page with Airbnb filters"""
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-    cities = storage.all(City).values()
-    sorted_cities = sorted(cities, key=lambda x: x.name)
-    amenities = storage.all(Amenity).values()
-    sorted_amenities = sorted(amenities, key=lambda x: x.name)
-    return render_template('10-hbnb_filters.html', states=sorted_states,
-                           cities=sorted_cities, amenities=sorted_amenities)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
