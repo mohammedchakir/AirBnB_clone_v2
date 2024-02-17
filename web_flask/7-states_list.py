d4098c76efab19b3fb04eaa6373edb729a08779a
mohammedchakir <zerroukimedchakir@gmail.com>
2024-02-17 20:52:26 +0100

    add

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,26 +2,26 @@
 """
 This script starts a Flask web application with specified routes.
 """
-
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
+
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_session(exception):
-    """Remove the current SQLAlchemy Session after each request"""
-    storage.close()
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
+@app.route("/states_list", strict_slashes=False)
+def display_states():
     """Display a list of all State objects present in DBStorage"""
     states = storage.all()
     return render_template('7-states_list.html', states=states)
 
 
-if __name__ == '__main__':
+@app.teardown_appcontext
+def teardown(self):
+    """Remove the current SQLAlchemy Session after each request"""
+    storage.close()
+
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
