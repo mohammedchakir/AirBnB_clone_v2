5f04b947401ae3a492fc040fdf81b5b56e128e94
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-21 04:17:27 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,35 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
+import models
 
 
 class State(BaseModel, Base):
-    """State class """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'states'
-        name = Column(String(128),
-                      nullable=False)
-        cities = relationship("City", cascade="all, delete",
-                              backref="states")
+    """ State class """
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade='all, delete')
     else:
         name = ""
 
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
-
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            """Attribute to retrieve City instances using the fs getter"""
-            values_city = models.storage.all("City").values()
-            list_city = []
-            for city in values_city:
-                if city.state_id == self.id:
-                    list_city.append(city)
-            return list_city
+            """getter for list of citis"""
+            new_list = []
+            all_cities = models.storage.all(City)
+            for key in all_cities:
+                if all_cities[key].state_id == self.id:
+                    new_list.append(all_cities[key])
+            return new_list
