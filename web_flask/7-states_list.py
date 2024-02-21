84d8e743b151e3363b980295ea87225461964a86
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-21 02:50:10 +0100

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,7 +4,8 @@ Script to start a Flask web application.
 """
 
 from flask import Flask, render_template
-from models import storage, State
+from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
