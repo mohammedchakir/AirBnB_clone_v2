7cb17b346790655c40d38052d651300c5b5837b5
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-21 03:34:47 +0100

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,27 +2,27 @@
 """
 Script to start a Flask web application.
 """
-
 from flask import Flask, render_template
-from models import storage
 from models.state import State
+from models import storage
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """Remove the current SQLAlchemy Session."""
+def teardown_storage(exception):
+    """ colses the connectetion """
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Display a HTML page with a list of all State objects sorted by name."""
+    """
+    the states_list route that renders all states from storage
+    """
     states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
